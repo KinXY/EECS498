30baf9ec8a2274d257684539411eb7965ec331ed
Ausummer <jinxiayang0422@gmail.com>
2024-04-25 14:40:05 +0800

    A1

--- ignore_large_files.ps1
+++ ignore_large_files.ps1
@@ -1,5 +1,5 @@
 $rootDir = Get-Location
 $files = Get-ChildItem -Recurse -File | Where-Object { $_.Length -gt 100MB }
-$relativePaths = $files.FullName.Replace($rootDir.ProviderPath, ".")
+$relativePaths = $files.FullName.Replace($rootDir, "").Replace("\", "/").TrimStart("/")
 
 Set-Content -Path ".gitignore" -Value $relativePaths
